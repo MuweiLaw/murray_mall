8c22a6e5be7d6c275b6a555b6c23e82475605bc1
Murray <2562758836@qq.com>
2021-12-07 19:12:10 +0800

    完成验证码

--- mall-shiro/src/main/java/com/murray/shiro/business/AdminBusiness.java
+++ mall-shiro/src/main/java/com/murray/shiro/business/AdminBusiness.java
@@ -82,18 +82,18 @@ public class AdminBusiness extends BaseController {
     }
 
     public ImageCodeDTO getImageCode(String uuid) {
-
         DefaultKaptcha producer = new DefaultKaptcha();
         Properties properties = new Properties();
         properties.setProperty("kaptcha.border", "yes");
-        properties.setProperty("kaptcha.border.color", "105,179,90");
-
+        properties.setProperty("kaptcha.border.color", "gray");
+        properties.setProperty("kaptcha.noise.color", "gray");
+        properties.setProperty("kaptcha.obscurificator.impl", "com.google.code.kaptcha.impl.WaterRipple");
         properties.setProperty("kaptcha.textproducer.font.color", "blue");
         properties.setProperty("kaptcha.textproducer.font.size", "32");
-        properties.setProperty("kaptcha.image.width", "100");
+        properties.setProperty("kaptcha.image.width", "150");
         properties.setProperty("kaptcha.image.height", "38");
-        properties.setProperty("kaptcha.textproducer.char.length", "4");
-        properties.setProperty("kaptcha.textproducer.font.names", "宋体,楷体,微软雅黑");
+        properties.setProperty("kaptcha.textproducer.char.length", "6");
+        properties.setProperty("kaptcha.textproducer.font.names", "楷体,微软雅黑,Arial,Courier");
 
         Config config = new Config(properties);
         producer.setConfig(config);
